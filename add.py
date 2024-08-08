1c4c030d8739bc64026a9afd46e7c97fd54c24da
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-08-08 21:47:15 +0800

    commit errors

--- add.py
+++ add.py
@@ -1,10 +1,11 @@
 def sum(x, y):
-    ...............
+    return .........
 
 def addition():
     a = int(input('Enter 1st number: '))
     b = int(input('Enter 2nd number: '))
-    print(f'Sum of {a} and {b} is ...........')
+    total = sum(.........)
+    print(f'Sum of {a} and {b} is {total}')
 
 ## JANGAN UBAH KOD DI BAWAH ##
 if __name__ == "__main__":
